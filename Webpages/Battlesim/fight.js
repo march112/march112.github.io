d44d30a18e8c88f8642f309a2b90f208f72d8ab4
MarcusC <marcus.a.chung@gmail.com>
2021-05-25 00:56:02 -0400

    Battlesim start

---
+++ Webpages/Battlesim/fight.js
@@ -0,0 +1,6 @@
+const words = new URLSearchParams(window.location.search);
+
+function speedComp (unit1speed, unit2speed){
+    let temp = str.trim()
+    return temp[0].toUpperCase() + temp.substring(1)
+  }
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+window.alert('Hello World');
 
 
 
