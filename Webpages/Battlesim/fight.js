8b8c9d95353bf97d984382f7009de80c3ea640f0
MarcusC <marcus.a.chung@gmail.com>
2021-12-27 13:42:06 -0500

    wheee

--- Webpages/Battlesim/fight.js
+++
@@ -1,6 +0,0 @@
-const words = new URLSearchParams(window.location.search);
-
-function speedComp (unit1speed, unit2speed){
-    let temp = str.trim()
-    return temp[0].toUpperCase() + temp.substring(1)
-  }
